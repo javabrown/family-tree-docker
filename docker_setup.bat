9631f2ff887678f15621ab2466916edfbe54f854
Raja Khan <rkhan@raileurope.com>
2017-03-28 16:35:31 -0400

    Initial Files

---
+++ docker_setup.bat
@@ -0,0 +1,5 @@
+docker login
+
+docker build -t getrk/family-tree .
+
+docker push
---
+++ src/index.php
@@ -0,0 +1,8 @@
+<html>
+  <h1>Hello PHP - Docker</h1>
+  <?php 
+    $msg = "DYNAMIC PHP TEXT DISPLAYED!!";
+     echo "<br/>Hello <b>$msg</b>";
+   ?>
+   
+</html>
